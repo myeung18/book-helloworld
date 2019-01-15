948600bc5f2a1fac54353ef4120d978db426e665
Marco Yeung <myeung@redhat.com>
2019-01-15 11:03:08 -0500

    java app added

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -8,20 +8,15 @@ import java.util.Random;
 @Path("/insult")
 public class InsultGenerator {
     public String generateInsult() {
-        String words[][] = {{"Artless", "Bawdy", "Beslubbering"},
-                {"Base-court", "Bat-fowling", "Beef-witted"}, {"Apple-john",
-                "Baggage", "Barnacle"}};
+        String words[][] = {{"Artless", "Bawdy", "Beslubbering"}, {"Base-court", "Bat-fowling", "Beef-witted"}, {"Apple-john", "Baggage", "Barnacle"}};
         String vowels = "AEIOU";
         String article = "an";
-        String firstAdjective = words[0][
-                new Random().nextInt(words[0].length)];
-        String secondAdjective = words[1][
-                new Random().nextInt(words[1].length)];
+        String firstAdjective = words[0][new Random().nextInt(words[0].length)];
+        String secondAdjective = words[1][new Random().nextInt(words[1].length)];
         String noun = words[2][new Random().nextInt(words[2].length)];
         if (vowels.indexOf(firstAdjective.charAt(0)) == -1) {
             article = "a";
         }
-        return String.format("Thou art %s %s %s %s!", article,
-                firstAdjective, secondAdjective, noun);
+        return String.format("Thou art %s %s %s %s!", article, firstAdjective, secondAdjective, noun);
     }
 }
